cff4bd0a644761ebd2dc1185f5eb622d3146b31f
a49serivce <a49service@gmail.com>
2025-07-22 17:08:49 +0900

    Add files via upload

--- app_research.js
+++ app_research.js
@@ -7,17 +7,14 @@ const researchsData = {
     description:
       "오디오파일 등급의 신호 전송을 위한 프리미엄 오디오 케이블",
     features: [
-      "은도금 OCC 구리 도체",
-      "다층 전자파 차폐",
-      "저인덕턴스 트위스트 페어 설계",
-      "정밀 수작업 조립"
+      "미정"
     ],
     specifications: {
-      도체: "99.99% 은도금 OCC 구리",
-      차폐: "쿼드 레이어 브레이드 차폐",
-      임피던스: "75Ω ±3Ω",
-      정전용량: "28pF/ft",
-      길이: "0.5m ~ 10m 맞춤 제작"
+      도체: "미정",
+      차폐: "미정",
+      임피던스: "미정",
+      정전용량: "미정",
+      길이: "미정"
     },
     image:
       "https://pplx-res.cloudinary.com/image/upload/v1751527759/pplx_project_search_images/db7e1ad503a2ac41eb663db448a720cc0deab747.jpg"
@@ -29,17 +26,14 @@ const researchsData = {
     description:
       "최적의 음향 환경을 위한 정밀 제작 음향 처리 솔루션",
     features: [
-      "천연 목재 베니어",
-      "고흡음 어쿠스틱 폼 코어",
-      "건축 통합 디자인",
-      "난연 소재 (Class A rated)"
+      "미정"
     ],
     specifications: {
-      NRC: "0.85 - 0.95",
-      두께: "25mm, 50mm, 75mm",
-      규격: "600x600mm, 1200x600mm",
-      마감: "오크, 월넛, 애쉬 베니어",
-      설치: "히든 마그넷 또는 스크류 마운트"
+      NRC: "미정",
+      두께: "미정",
+      규격: "미정",
+      마감: "미정",
+      설치: "미정"
     },
     image:
       "https://pplx-res.cloudinary.com/image/upload/v1751527759/pplx_project_search_images/04f81b42d97f64f673409c062f700cf410fdf69c.jpg"
@@ -51,17 +45,14 @@ const researchsData = {
     description:
       "깊고 정확한 저음 재생을 위한 레퍼런스 급 서브우퍼",
     features: [
-      "커스텀 롱쓰로우 드라이버",
-      "강성 MDF 캐비닛",
-      "고급 DSP 크로스오버",
-      "다양한 연결 옵션"
+      "미정"
     ],
     specifications: {
-      주파수_응답: "18Hz - 200Hz (±3dB)",
-      파워: "300W RMS / 1200W Peak",
-      드라이버_크기: "12인치, 15인치, 18인치",
-      SPL_max: "120dB @ 1m",
-      크기: "모델별 상이"
+      주파수_응답: "미정",
+      파워: "미정",
+      드라이버_크기: "미정",
+      SPL_max: "미정",
+      크기: "미정"
     },
     image:
       "https://pplx-res.cloudinary.com/image/upload/v1751527760/pplx_project_search_images/052037da93f428e52485b521558912090b5d030d.jpg"
